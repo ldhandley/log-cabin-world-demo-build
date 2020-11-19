d242f53200c5922dbf13638b8b74235ab4d61a29
srfoster <lindseyhandley@gmail.com>
2020-11-19 15:26:16 -0800

    preview

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "log-cabin-world-demo-build")
 (define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("racket-doc" "rackunit-lib"))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(ThoughtSTEM))
